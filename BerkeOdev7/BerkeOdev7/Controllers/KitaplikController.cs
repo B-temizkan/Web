f5593df53841309c7aa177296752a43135160123
B-temizkan <60464835+B-temizkan@users.noreply.github.com>
2020-12-07 22:05:34 +0300

    Web mvc

---
+++ BerkeOdev7/BerkeOdev7/Controllers/KitaplikController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BerkeOdev7.Controllers
+{
+    public class KitaplikController : Controller
+    {
+        // GET: Kitaplik
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Delete(int Id)
+        {
+            var Kitap = Models.KitapVeri.VeriListe.Where(x => x.Id == Id).FirstOrDefault();
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Delete(Models.KitapVeri kitapVeri)
+        {
+            var Kitap = Models.KitapVeri.VeriListe.FirstOrDefault(x => x.Id == kitapVeri.Id);
+            Models.KitapVeri.VeriListe.Remove(Kitap);
+            return RedirectToAction("Sirala");
+        }
+        public ActionResult Edit (int Id)
+        {
+            var kitap = Models.KitapVeri.VeriListe.Where(x => x.Id == Id).FirstOrDefault();
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Edit(Models.KitapVeri kitapVeri)
+        {
+            var kitap = Models.KitapVeri.VeriListe.FirstOrDefault(x => x.Id == kitapVeri.Id);
+            kitap.Ad = kitapVeri.Ad;
+            kitap.Yazar = kitapVeri.Yazar;
+            kitap.YayinEvi = kitapVeri.YayinEvi;
+            kitap.Fiyat = kitapVeri.Fiyat;
+            kitap.BasimYil = kitapVeri.BasimYil;
+            return RedirectToAction("Sirala");
+        }
+        public ActionResult Sirala()
+        {
+            return View(Models.KitapVeri.VeriListe);
+        }
+    }
+}
---
+++ BerkeOdev7/BerkeOdev7/Models/Kitap.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BerkeOdev7.Models
+{
+    public class KitapVeri
+    {
+        public int Id { get; set; }
+        public string Ad { get; set; }
+        public string Yazar { get; set; }
+        public string YayinEvi { get; set; }
+        public long Fiyat { get; set; }
+        public short BasimYil { get; set; }
+
+
+        public static List<KitapVeri> VeriListe = new List<KitapVeri>
+        {
+            new KitapVeri
+            {
+                Id=1,
+                Ad="Son Ada",
+                Yazar="Zülfü Livaneli",
+                YayinEvi="Doğan Kitap",
+                Fiyat=20,
+                BasimYil=2019,
+
+            },
+            new KitapVeri
+            {
+                Id=2,
+                Ad="Kayıp Sembol",
+                Yazar="Dan Brown",
+                YayinEvi="Altın Kitaplar",
+                Fiyat=25,
+                BasimYil=2006,
+
+            },
+             new KitapVeri
+            {
+                Id=3,
+                Ad="Cehennem Kapıları",
+                Yazar="Dan Brown",
+                YayinEvi="Pegasus",
+                Fiyat=15,
+                BasimYil=2008,
+
+            },
+              new KitapVeri
+            {
+                Id=3,
+                Ad="Cehennem",
+                Yazar="Dan Brown",
+                YayinEvi="Altın Kitaplar",
+                Fiyat=35,
+                BasimYil=2004,
+
+            },
+              new KitapVeri
+            {
+                Id=4,
+                Ad="Karanlık Sular",
+                Yazar="Paula Hawkıns",
+                YayinEvi="İthaki",
+                Fiyat=55,
+                BasimYil=2016,
+
+            },
+              new KitapVeri
+            {
+                Id=5,
+                Ad="Trendeki Kız",
+                Yazar="Paula Hawkıns",
+                YayinEvi="İthaki",
+                Fiyat=45,
+                BasimYil=2014,
+
+            },
+        };
+
+    }
+}
